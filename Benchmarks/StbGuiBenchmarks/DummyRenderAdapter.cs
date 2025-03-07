4ff27c8e128f8c94a8ce1f539c3b16d8089265b7
Federico D'Angelo <federicodangelo@gmail.com>
2025-03-06 23:14:06 -0300

    Fixed benchmark build

--- Benchmarks/StbGuiBenchmarks/DummyRenderAdapter.cs
+++ Benchmarks/StbGuiBenchmarks/DummyRenderAdapter.cs
@@ -9,7 +9,6 @@ public class DummyRenderAdapter : StbGuiRenderAdapter
 
     public void destroy_texture(nint texture_id)
     {
-
     }
 
     public void draw_rects(StbGuiRenderAdapter.Rect[] rects, int count, nint texture_id)
@@ -18,21 +17,25 @@ public class DummyRenderAdapter : StbGuiRenderAdapter
 
     public void draw_vertices(StbGuiRenderAdapter.Vertex[] vertices, int count, nint texture_id)
     {
-        throw new NotImplementedException();
     }
 
     public void pop_clip_rect()
     {
+    }
 
+    public void process_render_command(StbGui.stbg_render_command cmd)
+    {
     }
 
     public void push_clip_rect(StbGui.stbg_rect rect)
     {
+    }
 
+    public void register_font(int font_id, StbGuiFont font)
+    {
     }
 
     public void set_texture_pixels(nint texture_id, StbGui.stbg_size size, byte[] pixels)
     {
-
     }
 }
